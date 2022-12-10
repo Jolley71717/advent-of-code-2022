14222cf1833159a4360570ac919ab2e8dbf91239
Luke Dutton <duttonluke03@gmail.com>
2022-12-10 00:48:18 -0700

    updated tests

--- day2/src/test/kotlin/com/jolley71717/TestRounds.kt
+++ day2/src/test/kotlin/com/jolley71717/TestRounds.kt
@@ -1,20 +1,21 @@
 package com.jolley71717
 
 import com.jolley71717.RockPaperScissorsUtil.firstPassTotalScore
+import com.jolley71717.RockPaperScissorsUtil.updatedStrategy
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class TestRounds {
     @Test
     fun testCasesTotalScore() {
-        val highestCalories = testData.firstPassTotalScore()
-        assertEquals(15L, highestCalories)
+        val totalScore = testData.firstPassTotalScore()
+        assertEquals(15L, totalScore)
     }
 
     @Test
     fun testBetterIncrement() {
-//        val highestC3alories = testData.highestTopXTotal(3)
-//        assertEquals(45000L, highestC3alories)
+        val totalScore = testData.updatedStrategy()
+        assertEquals(12L, totalScore)
     }
 
     companion object {
